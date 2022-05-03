b54ed97f3f4bd4f4901a2d75a5e5ed37e20e05cf
nazm0l <tanjirahmednazmul@gmail.com>
2022-05-04 01:07:25 +0600

    get single car added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -26,6 +26,13 @@ async function run(){
 
                 res.send(cars);
             });
+
+            app.get('/cars/:id', async(req, res) =>{
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const cars = await carCollection.findOne(query);
+                res.send(cars);
+              });
         }
         finally{
 
